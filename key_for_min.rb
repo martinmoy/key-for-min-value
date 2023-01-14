7abc8b4d4e31e0457e461280c38132cc5011a31f
Martin Moy <martinmoy1991@gmail.com>
2023-01-13 21:54:43 -0500

    commit tree 035997c7577a78edc53ece9cb6a80572b7963f54

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  #need something to compare
+  lowest_value = 0
+  
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
